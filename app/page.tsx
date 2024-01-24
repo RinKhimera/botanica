2f0b71cdca5961c5d4d71e302d114b9b9ff3b650
Samuel Pokam <dixiades@gmail.com>
2024-01-24 16:25:21 +0100

    1st commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Botanica</h1>
+    </main>
+  )
+}
